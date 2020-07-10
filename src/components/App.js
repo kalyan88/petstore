fbb587e546765f55072350f553753b2918aa0597
Kalyan Gurugubelli <kalyangurugubelli@Kalyans-MacBook-Pro.local>
2020-07-10 19:03:31 -0400

    petstore first commit

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main className="page bg-white" id="petratings">
+       <div className="container">
+         <div className="row">
+           <div className="col-md-12 bg-white">
+             <div className="container">
+               <div>Add Appointment</div>
+               <div>Search Appointment</div>
+               <div>List Appointment</div>
+             </div>
+           </div>
+         </div>
+       </div>
+     </main>
+    </div>
+  );
+}
+
+export default App;
