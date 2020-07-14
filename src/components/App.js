ad4884412427173e596684595a0c43015462d642
Kalyan Gurugubelli <kalyangurugubelli@Kalyans-MacBook-Pro.local>
2020-07-14 18:41:29 -0400

    created 3 simple component 07-14

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../css/App.css';
+import AddAppointments from './AddAppointments';
+import ListAppointments from './ListAppointments'
+import SearchAppointments from './SearchAppointments'
 
-function App() {
-  return (
-    <div className="App">
-      <main className="page bg-white" id="petratings">
-       <div className="container">
-         <div className="row">
-           <div className="col-md-12 bg-white">
-             <div className="container">
-               <div>Add Appointment test</div>
-               <div>Search Appointment</div>
-               <div>List Appointment</div>
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <main className="page bg-white" id="petratings">
+         <div className="container">
+           <div className="row">
+             <div className="col-md-12 bg-white">
+               <div className="container">
+                 <AddAppointments />
+                 <ListAppointments />
+                 <SearchAppointments />
+               </div>
              </div>
            </div>
          </div>
-       </div>
-     </main>
-    </div>
-  );
+       </main>
+      </div>
+    );
+  }
 }
+  
 
 export default App;
---
+++ src/components/SearchAppointments.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+
+class SearchAppointments extends Component {
+    render() {
+        return <div>Search Appointments from Component</div>
+    }
+ }
+ export default SearchAppointments
